bc531e6d83de8a7e20aa8b2069857cd72e44b9a9
Thinkgoez <thinkgoez@gmail.com>
2021-01-06 12:27:18 +0200

    2, using tabs in SimpleRouting

--- src/components/SimpleRouting.js
+++ src/components/SimpleRouting.js
@@ -5,20 +5,60 @@ import {
     Link,
 } from "react-router-dom";
 
+
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 224,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
 export default function Example1({ match }) {
     let { url } = match
+    const classes = useStyles();
+
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <Router basename={url}>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/users'>Users</Link></li>
-            </ul>
-            <Switch>
-                <Route path='/about'><About /></Route>
-                <Route path='/users'><Users /></Route>
-                <Route path='/'><Home /></Route>
-            </Switch>
+            <div className={classes.root}>
+                <Tabs
+                    orientation="vertical"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    className={classes.tabs}
+                    variant='standart'
+                >
+                    <Tab label={<Link to='/'>Home</Link>} />
+                    <Tab label={<Link to='/about'>About</Link>} />
+                    <Tab label={<Link to='/users'>Users</Link>} />
+                </Tabs>
+                <Box p={3}>
+                    <Switch>
+                        <Route path='/about'><About /></Route>
+                        <Route path='/users'><Users /></Route>
+                        <Route path='/'><Home /></Route>
+                    </Switch>
+                </Box>
+            </div>
         </Router>
     );
 }
